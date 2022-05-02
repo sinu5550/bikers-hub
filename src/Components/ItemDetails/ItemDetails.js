2587338f4c55cdfc27816b1cdf2e9b51ba2b48be
Intisar Ahmed Siyan <intisar.siyan5550@gmail.com>
2022-05-03 03:33:14 +0600

    Items id route designing

--- src/Components/ItemDetails/ItemDetails.js
+++ src/Components/ItemDetails/ItemDetails.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import useItemDetails from '../Hooks/useItemDetails';
+import brandLogo from '../../Images/bikers-hub.png';
 
 const ItemDetails = () => {
     const { itemsId } = useParams();
@@ -8,6 +9,32 @@ const ItemDetails = () => {
     return (
         <div>
             <h1 className='mt-28 font-bold text-2xl text-center'>You are about to book: {inventory.productName}</h1>
+            <div className='shadow-lg w-11/12 mx-auto p-4  bg-white self-auto card '>
+                <div className='grid grid-cols-2 '>
+                    <div className='w-full col-span-2 md:col-span-1'>
+                        <img className='mx-auto' src={inventory.img} alt={inventory.productName} />
+                    </div>
+                    <div className='md:pr-28 my-auto col-span-2 md:col-span-1'>
+                        <div className='mx-auto'>
+                            <div className='flex justify-between'>
+                                <h1 className=' font-bold font-[poppins] uppercase text-5xl'>
+                                    {inventory.productName}
+                                </h1>
+                                <div>
+                                    <img src={brandLogo} width='150px' alt="BIKERS_HUB" />
+                                </div>
+                            </div>
+                            <p> <span className='font-bold '>Price:</span>  BDT {inventory.price}</p>
+                            <h1 className='font-bold text-2xl '>
+                                About {inventory.productName}
+                            </h1>
+                            <p>{inventory.description}</p>
+                            <p> <span className='font-bold '> Quantity:</span> {inventory.quantity}</p>
+                            <p > <span className='font-bold'>Supplier Name: </span> {inventory.supplier}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </div>
     );
--- src/Components/ManageItems/ManageItems.js
+++ src/Components/ManageItems/ManageItems.js
@@ -6,7 +6,7 @@ const ManageItems = () => {
     const handleDelete = id => {
         const proceed = window.confirm('Are yo sure? You want to delete this item?');
         if (proceed) {
-            const url = `http://localhost:5000/inventory${id}`;
+            const url = `https://fast-plains-59234.herokuapp.com/inventory/${id}`;
             fetch(url, {
                 method: 'DELETE'
             })
@@ -20,13 +20,15 @@ const ManageItems = () => {
     }
     return (
         <div>
-            <div className='w-full'>
+            <div className='w-full pt-20'>
+
                 <h1>Manage your Inventory Items</h1>
                 {
-                    inventory.map(inventory => <div key={inventory._id}>
-                        <p>{inventory.productName} {inventory.description} <button onClick={() => handleDelete(inventory._id)}>delete</button> </p>
+                    inventory.map(inventory => <div className='bg-white' key={inventory._id}>
+                        <p>{inventory.productName} {inventory.description} <button onClick={() => handleDelete(inventory._id)} className='bg-red-900'>delete</button> </p>
                     </div>)
                 }
+
             </div>
         </div>
     );
